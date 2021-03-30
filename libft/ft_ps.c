db751dd143fde5b8f05a7bdb53da72196d8e1fc5
Alexdelia <alexandre.delille.57@gmail.com>
2021-03-30 12:08:38 +0200

    fixed

---
+++ libft/ft_ps.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ps.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/26 13:09:31 by adelille          #+#    #+#             */
+/*   Updated: 2021/03/20 15:36:02 by adelille         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_ps(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	write(1, str, i);
+	return (i);
+}
+
+int	ft_psc(char *str, char *color)
+{
+	write(1, color, ft_strlen(color));
+	write(1, str, ft_strlen(str));
+	write(1, DEF, ft_strlen(DEF));
+	return (ft_strlen(color) + ft_strlen(str) + ft_strlen(DEF));
+}
