cc5bfd455cf1e6d89a028584ef351103bfc3a3f3
Alexdelia <alexandre.delille.57@gmail.com>
2021-04-02 08:04:17 +0200

    improve echo

--- srcs/ft_echo.c
+++ srcs/ft_echo.c
@@ -6,7 +6,7 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/30 17:41:22 by adelille          #+#    #+#             */
-/*   Updated: 2021/04/01 12:56:04 by adelille         ###   ########.fr       */
+/*   Updated: 2021/04/02 08:02:02 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,17 +30,6 @@ static int	ft_is_n(char *str)
 	return (FALSE);
 }
 
-static int	ft_has_n(t_word *word)
-{
-	while (word)
-	{
-		if (ft_is_n(word->data) == TRUE)
-			return (TRUE);
-		word = word->next;
-	}
-	return (FALSE);
-}
-
 static void	ft_echo_env_convert(int fd, char *str, t_env *env)
 {
 	ft_putstr_fd(ft_env_search(&str[1], env), fd);
@@ -51,17 +40,21 @@ int			ft_echo(int fd, t_word *word, t_env *env)
 	t_word	*head;
 
 	head = word;
+	while (ft_is_n(word->data))
+		word = word->next;
 	while (word)
 	{
 		if (word->data[0] == '$')
 			ft_echo_env_convert(fd, word->data, env);
-		else if (ft_is_n(word->data) == FALSE)
+		else
 			ft_putstr_fd(word->data, fd);
 		word = word->next;
+		if (word)
+			ft_putstr_fd(" ", fd);
 	}
 	word = head;
 	//ft_lstclear(head, free);
-	if (ft_has_n(word) == FALSE)
+	if (ft_is_n(word->data) == FALSE)
 		ft_putstr_fd("\n", fd);
 	if (fd != STDOUT)
 		close(fd);
