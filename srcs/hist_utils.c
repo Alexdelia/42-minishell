debcafaef3d950eedf607bd17954fd8ab3426808
nessayan <nessayan@student.42.fr>
2021-04-02 17:44:36 +0200

    prompt

--- srcs/hist_utils.c
+++ srcs/hist_utils.c
@@ -6,7 +6,7 @@
 /*   By: nessayan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/31 10:13:03 by nessayan          #+#    #+#             */
-/*   Updated: 2021/03/31 11:05:34 by nessayan         ###   ########.fr       */
+/*   Updated: 2021/04/02 15:59:13 by nicolases        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,29 +17,18 @@ t_hist	*new_hist(char *str)
 	t_hist	*new;
 
 	new = malloc(sizeof(t_hist));
-	new->data = ft_strdup(str);
+	if (str == NULL)
+		new->data = NULL;
+	else
+		new->data = ft_strdup(str);
 	new->next = NULL;
 	return (new);
 }
 
-t_hist	*get_last_hist(t_hist *hist)
+void	add_front_hist(t_hist **hist, t_hist *new)
 {
-	if (hist == NULL)
-		return (hist);
-	while (hist->next != NULL)
-		hist = hist->next;
-	return (hist);
-}
-
-void	add_back_hist(t_hist **hist, t_hist *new)
-{
-	t_hist	*l;
-
-	l = get_last_hist(*hist);
-	if (l != NULL)
-		l->next = new;
-	else
-		*hist = new;
+	new->next = *hist;
+	*hist = new;
 }
 
 void	store_hist(t_data *d, char *l)
@@ -47,13 +36,16 @@ void	store_hist(t_data *d, char *l)
 	if (d->hist == NULL)
 		d->hist = new_hist(l);
 	else
-		add_back_hist(&d->hist, new_hist(l));
+		add_front_hist(&d->hist, new_hist(l));
 }
 
 void	free_one_hist(t_hist *hist)
 {
-	free(hist->data);
-	hist->data = NULL;
+	if (hist->data != NULL)
+	{
+		free(hist->data);
+		hist->data = NULL;
+	}
 	hist->next = NULL;
 	free(hist);
 	hist = NULL;
