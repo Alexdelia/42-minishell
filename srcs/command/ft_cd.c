ae235a0b55f61bce0089029a35f04baffc46dab1
Alexdelia <alexandre.delille.57@gmail.com>
2021-04-09 12:06:52 +0200

    norme

--- srcs/command/ft_cd.c
+++ srcs/command/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/30 18:19:32 by adelille          #+#    #+#             */
-/*   Updated: 2021/04/09 11:55:21 by adelille         ###   ########.fr       */
+/*   Updated: 2021/04/09 12:01:54 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@
 int	ft_cd(char *path)
 {
 	if (chdir(path) == -1)
-		return (ft_mi_error("cd", "Path not found", 1));
+		return (ft_mi_error("cd", "path not found", 1));
 	return (0);
 }
