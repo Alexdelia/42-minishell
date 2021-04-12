22f11821f817bfaaabbd38aae8580cec3113e643
Alexdelia <alexandre.delille.57@gmail.com>
2021-04-12 16:58:41 +0200

    fix ft_mi_strcat

--- srcs/command/ft_echo.c
+++ srcs/command/ft_echo.c
@@ -6,11 +6,24 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/30 17:41:22 by adelille          #+#    #+#             */
-/*   Updated: 2021/04/08 14:39:49 by adelille         ###   ########.fr       */
+/*   Updated: 2021/04/12 16:51:01 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+/*static int	ft_ps_but_one_c(char *str, char c, int fd)
+{
+	int	is_c;
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		
+		i++;
+	}
+}*/
 
 int			ft_echo(int fd, t_word *word)
 {
@@ -27,7 +40,7 @@ int			ft_echo(int fd, t_word *word)
 		else
 			i++;
 	}
-	ft_putstr_fd(&word->data[i], fd);
+	//ft_ps_but_one_c(&word->data[i], ' ', fd);
 	i = 0;
 	if (word->data[0] == '-')
 	{
