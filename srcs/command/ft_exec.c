22fc348df8770ee84b7d6a4b8eb3c1235ecb917f
Alexdelia <alexandre.delille.57@gmail.com>
2021-04-13 20:33:58 +0200

    refactoring and norming

--- srcs/command/ft_exec.c
+++ srcs/command/ft_exec.c
@@ -6,28 +6,18 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/31 14:14:43 by adelille          #+#    #+#             */
-/*   Updated: 2021/04/13 18:19:57 by nicolases        ###   ########.fr       */
+/*   Updated: 2021/04/13 20:33:03 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	ft_exec(t_word *word, t_env *env, int fd)
+static char	**ft_create_av(t_word *word)
 {
-	char		**av;
-	char		**envp;
-	char		*join_f;
-	char		*join_e;
-	int			pid;
-	int		pid;
-	struct stat	stats;
-	int		exec_status;
+	char	**av;
+	char	*join_f;
+	char	*join_e;
 
-	exec_status = 0;
-	/*pipe(pfd);*/
-	if (stat(word->data, &stats) == -1)
-	  return (ft_mi_error(word->data, "No such file or directory", 127));
-	envp = etoa(env);
 	join_f = ft_strjoin(word->data, " ");
 	if (word->next != NULL)
 		join_e = ft_strjoin(join_f, word->next->data);
@@ -36,6 +26,23 @@ int	ft_exec(t_word *word, t_env *env, int fd)
 	av = ft_split(join_e, ' ');
 	free(join_f);
 	free(join_e);
+	return (av);
+}
+
+int			ft_exec(t_word *word, t_env *env, int fd)
+{
+	char		**av;
+	char		**envp;
+	int			pid;
+	int			exec_status;
+	struct stat	stats;
+
+	exec_status = 0;
+	/*pipe(pfd);*/
+	if (stat(word->data, &stats) == -1)
+	  return (ft_mi_error(word->data, "No such file or directory", 127));
+	envp = etoa(env);
+	av = ft_create_av(word);
 	pid = fork();
 	if (pid == 0)
 	{
