e6c5cc5da5671aa00ed945ce2a99285b01d1b854
Alexdelia <alexandre.delille.57@gmail.com>
2021-04-21 15:19:57 +0200

    fix error prone at start

--- srcs/utils/ft_parse_case.c
+++ srcs/utils/ft_parse_case.c
@@ -6,7 +6,7 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/20 11:12:00 by adelille          #+#    #+#             */
-/*   Updated: 2021/04/21 15:11:09 by adelille         ###   ########.fr       */
+/*   Updated: 2021/04/21 15:19:24 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,11 @@
 
 int		ft_parse_case(t_data *d, char *line, int c)
 {
+	int		i;
 	int		process_num;
 	char	char_stop;
 
+	i = 0;
 	process_num = 0;
 	char_stop = 0;
 	while (process_num < c)
@@ -32,15 +34,21 @@ int		ft_parse_case(t_data *d, char *line, int c)
 				else if (char_stop == '|')
 					return (ft_mi_error("syntax error near  unexpected token",
 						"'|'", FALSE));
-				else if (char_stop == '>')
-					return (ft_mi_error("syntax error near  unexpected token",
-						"'>'", FALSE));
-				else if (char_stop == 'C')
-					return (ft_mi_error("syntax error near  unexpected token",
-						"'>>'", FALSE));
-				else if (char_stop == '<')
-					return (ft_mi_error("syntax error near  unexpected token",
-						"'<'", FALSE));
+				while (line[i] && (line[i] == ' '
+						|| line[i] == '>' || line[i] == '<'))
+					i++;
+				if (!line[i])
+				{
+					if (char_stop == '>')
+						return (ft_mi_error("syntax error near  unexpected token",
+							"'>'", FALSE));
+					else if (char_stop == 'C')
+						return (ft_mi_error("syntax error near  unexpected token",
+							"'>>'", FALSE));
+					else if (char_stop == '<')
+						return (ft_mi_error("syntax error near  unexpected token",
+							"'<'", FALSE));
+				}
 			}	
 		}
 		ft_free_all_word(d->word);
