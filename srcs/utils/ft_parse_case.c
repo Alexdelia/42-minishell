36cfef6e38097eca0110b6608a07d7b7fab78838
Alexdelia <alexandre.delille.57@gmail.com>
2021-04-21 16:31:40 +0200

    refacto to norme ft_parse_case

--- srcs/utils/ft_parse_case.c
+++ srcs/utils/ft_parse_case.c
@@ -6,18 +6,56 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/20 11:12:00 by adelille          #+#    #+#             */
-/*   Updated: 2021/04/21 16:24:08 by adelille         ###   ########.fr       */
+/*   Updated: 2021/04/21 16:31:02 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int		ft_parse_case(t_data *d, char *line, int c)
+static int	ft_chevron_case(char *line, int i, char char_stop)
+{
+	if (char_stop == '>')
+		return (ft_mi_error("syntax error near  unexpected token",
+					"'>'", FALSE));
+	else if (char_stop == 'C')
+		return (ft_mi_error("syntax error near  unexpected token",
+					"'>>'", FALSE));
+	else if (char_stop == '<')
+		return (ft_mi_error("syntax error near  unexpected token",
+					"'<'", FALSE));
+	return (TRUE);
+}
+
+static int	ft_case(t_data *d, char *line, int i)
+{
+	char	char_stop;
+
+	if (!d->word->data[0])
+	{
+		char_stop = ft_char_stop(line, process_num);
+		if (char_stop == ';')
+			return (ft_mi_error("syntax error near unexpected token",
+						"';'", FALSE));
+		else if (char_stop == '|')
+			return (ft_mi_error("syntax error near  unexpected token",
+						"'|'", FALSE));
+		while (line[i] && (line[i] == ' '
+					|| line[i] == '>' || line[i] == '<'))
+			i++;
+		if (!line[i])
+		{
+			if (ft_chevron_case(line, i, char_stop) == FALSE)
+				return (FALSE);
+		}
+	}
+	return (TRUE);
+}
+
+int			ft_parse_case(t_data *d, char *line, int c)
 {
 	int		status;
 	int		i;
 	int		process_num;
-	char	char_stop;
 
 	i = 0;
 	process_num = 0;
@@ -25,33 +63,10 @@ int		ft_parse_case(t_data *d, char *line, int c)
 	while (process_num < c)
 	{
 		status = ft_word_split(d, line, process_num);
-		if(status == 0)
+		if (status == 0)
 		{
-			if (!d->word->data[0])
-			{
-				char_stop = ft_char_stop(line, process_num);
-				if (char_stop == ';')
-					return (ft_mi_error("syntax error near unexpected token",
-						"';'", FALSE));
-				else if (char_stop == '|')
-					return (ft_mi_error("syntax error near  unexpected token",
-						"'|'", FALSE));
-				while (line[i] && (line[i] == ' '
-						|| line[i] == '>' || line[i] == '<'))
-					i++;
-				if (!line[i])
-				{
-					if (char_stop == '>')
-						return (ft_mi_error("syntax error near  unexpected token",
-							"'>'", FALSE));
-					else if (char_stop == 'C')
-						return (ft_mi_error("syntax error near  unexpected token",
-							"'>>'", FALSE));
-					else if (char_stop == '<')
-						return (ft_mi_error("syntax error near  unexpected token",
-							"'<'", FALSE));
-				}
-			}	
+			if (ft_case(d, line, i) == FALSE)
+				return (FALSE);
 		}
 		ft_free_all_word(d->word);
 		d->word = NULL;
