146bc4d5f04746abf92092085be912852a244b0a
Alexdelia <alexandre.delille.57@gmail.com>
2021-04-25 14:33:56 +0200

    norme semi, everything is normed

--- srcs/engine/semi.c
+++ srcs/engine/semi.c
@@ -6,11 +6,25 @@
 /*   By: nicolasessayan <marvin@42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/23 21:47:06 by nicolases         #+#    #+#             */
-/*   Updated: 2021/04/23 21:47:09 by nicolases        ###   ########.fr       */
+/*   Updated: 2021/04/25 14:32:57 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+static void	ft_end_semi(char *line, t_data *d, int process_num, int **pfd)
+{
+	if (process_num > 0 && ft_char_stop(line, process_num - 1) == '|')
+	{
+		close(pfd[process_num - 1][1]);
+		dup2(pfd[process_num - 1][0], 0);
+	}
+	if (process_num == 0 || (ft_char_stop(line, process_num - 1) != '>'
+				&& ft_char_stop(line, process_num - 1) != 'C'
+				&& ft_char_stop(line, process_num - 1) != '<'))
+		ft_parse_exec(d->word, d);
+	exit(g_status);
+}
 
 void		ft_semi(char *line, t_data *d, int process_num, int **pfd)
 {
@@ -19,18 +33,7 @@ void		ft_semi(char *line, t_data *d, int process_num, int **pfd)
 
 	pid = fork();
 	if (pid == 0)
-	{
-		if (process_num > 0 && ft_char_stop(line, process_num - 1) == '|')
-		{
-			close(pfd[process_num - 1][1]);
-			dup2(pfd[process_num - 1][0], 0);
-		}
-		if (process_num == 0 || (ft_char_stop(line, process_num - 1) != '>'
-			&& ft_char_stop(line, process_num - 1) != 'C'
-			&& ft_char_stop(line, process_num - 1) != '<'))
-			ft_parse_exec(d->word, d);
-		exit(g_status);
-	}
+		ft_end_semi(line, d, process_num, pfd);
 	else
 	{
 		waitpid(pid, &stats, 0);
