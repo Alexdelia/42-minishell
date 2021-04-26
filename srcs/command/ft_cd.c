ae381a23b305a1919e07f158823a010a224cf3dc
Alexdelia <alexandre.delille.57@gmail.com>
2021-04-26 10:50:04 +0200

    add empty string support with cd

--- srcs/command/ft_cd.c
+++ srcs/command/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/30 18:19:32 by adelille          #+#    #+#             */
-/*   Updated: 2021/04/26 10:41:50 by adelille         ###   ########.fr       */
+/*   Updated: 2021/04/26 10:48:53 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 int	ft_cd(char *path)
 {
+	if (!path[0])
+		return (0);
 	if (chdir(path) == -1)
 		return (ft_mi_error("cd", "No such file or directory", 1));
 	return (0);
