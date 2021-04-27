92df9073d1f38f604517748cdfcc54b7e92fbf50
Alexdelia <alexandre.delille.57@gmail.com>
2021-04-27 15:34:29 +0200

    fix g_status = 2 when syntax error

--- srcs/utils/redirection_syntax_error.c
+++ srcs/utils/redirection_syntax_error.c
@@ -6,7 +6,7 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/25 11:56:34 by adelille          #+#    #+#             */
-/*   Updated: 2021/04/27 15:27:17 by adelille         ###   ########.fr       */
+/*   Updated: 2021/04/27 15:33:45 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 static int	ft_syntax_message(char *type)
 {
-	g_status = 1;
+	g_status = 2;
 	ft_pserc("minishell: redirection: syntax error near unexpected token ",
 		RED);
 	ft_pserc(type, RED);
